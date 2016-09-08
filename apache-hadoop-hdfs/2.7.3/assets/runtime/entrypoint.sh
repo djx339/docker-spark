46c779952d6fb8566a81e435fbf25ab3969d95b9
Daniel D <djx339@gmail.com>
2016-09-08 22:24:46 +0800

    Update entrypoint.sh

--- apache-hadoop-hdfs/2.7.3/assets/runtime/entrypoint.sh
+++ apache-hadoop-hdfs/2.7.3/assets/runtime/entrypoint.sh
@@ -21,6 +21,8 @@ hadoop_configure_common() {
     chmod go-rx /root/.ssh
     chmod go-rwx /root/.ssh/id_rsa
     chmod go-wx /root/.ssh/authorized_keys
+    
+    sed -i 's/^export JAVA_HOME.*$/export JAVA_HOME="'"${JAVA_HOME}"'"/g' $HADOOP_CONF_DIR/hadoop-env.sh
 }
 
 hadoop_configure_hdfs() {
